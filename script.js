5b7d82dec434b1d3e803106fbca65ee687447b8a
LuKa44x <lucavicari44@gmail.com>
2025-04-03 16:07:00 +0200

    Functions for basic math
    Created the 4 basic math functions
    Created function and variables at the start just to test if works well.

---
+++ script.js
@@ -0,0 +1,38 @@
+let aTest = 3;
+let bTest = 5;
+let operator = "";
+
+function operate(aTest , bTest){
+    return add(aTest,bTest);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
